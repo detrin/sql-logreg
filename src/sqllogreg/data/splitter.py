10b38125821ee2176d7b0c76f6b024ce8ea9d0d4
Daniel Herman <danherma@cisco.com>
2026-03-01 01:46:14 +0100

    feat: add splitter

---
+++ src/sqllogreg/data/splitter.py
@@ -0,0 +1,5 @@
+from sklearn.model_selection import train_test_split
+
+class Splitter:
+    def split(self, X, y, test_size=0.2):
+        return train_test_split(X, y, test_size=test_size, random_state=42)
